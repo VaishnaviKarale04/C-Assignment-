6ae51a47549d257f57c705c6934126e77648ddee
Vaishnavi karale <karalepatilvaishnavi@gmail.com>
2026-07-15 21:22:30 +0530

    19 Square root Of Given Number.c

---
+++ 01_Assignment/19 Square root Of Given Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+    {
+       int  Num = 0;
+       float SquareR = 0;
+
+       printf("\n Enter a Number : ");
+       scanf("%d",&Num);
+
+       SquareR = sqrt(Num);
+
+       printf("\n Squaroot Of Given Number : %.2f", SquareR);
+
+       getch();
+       return 0;
+    }
